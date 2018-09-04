89fbf69b61c0e31d9b65675d409fe3eb2c8073ee
Steve Biedermann <hardliner66@gmail.com>
2018-09-04 09:38:25 +0200

    (+) -n|--no-reboot flag

--- install.sh
+++ install.sh
@@ -1,1 +1,17 @@
-sudo -- sh -c 'cp configuration.nix /etc/nixos/configuration.nix && nixos-rebuild switch && reboot'
+DO_REBOOT=1
+for i in "$@"
+do
+case $i in
+    -n|--no-reboot)
+    DO_REBOOT=0
+    ;;
+    *)
+    ;;
+esac
+done
+
+if [ "$DO_REBOOT" -eq "1" ]; then
+    sudo -- sh -c 'cp configuration.nix /etc/nixos/configuration.nix && nixos-rebuild switch && reboot';
+else
+    sudo -- sh -c 'cp configuration.nix /etc/nixos/configuration.nix && nixos-rebuild switch';
+fi
